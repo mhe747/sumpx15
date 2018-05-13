7f56fe8beede8f7b90f6f97dc3779b972fa9b483
Mich Mich <mhe747@gmail.com>
2018-05-14 01:07:01 +0200

    Update run.bat

--- client/run.bat
+++ client/run.bat
@@ -1,1 +1,6 @@
-java -jar analyzer.jar %1 %2
+cd bin
+jar xf ../lib/RXTXcomm.jar
+jar cfm analyzer.jar Manifest.txt org gnu
+if not exist rxtxSerial.dll copy ..\lib\rxtxSerial.dll .
+java -Djava.library.path=. -jar analyzer.jar
+REM java -Djava.library.path=. -jar analyzer.jar %1 %2
